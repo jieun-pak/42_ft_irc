4d094474641f8b22f2e3031bea3a40a9213bee97
Jin <ahnwia@gmail.com>
2026-07-18 21:37:23 +0200

    fix: enable compilation and sanitize server code

--- ircserv/srcs/Client.cpp
+++ ircserv/srcs/Client.cpp
@@ -6,11 +6,15 @@ const std::string &Message::getCommand() const
 {
 	return command;
 }
+const std::vector<std::string> &Message::getParams() const
+{
+	return params;
+}
 
 // constructors
-Client::Client(int fd) : _fd(fd), _passwordAuthenticated(false), _isUserReceived(false), _nickname(""), _username(""), _realname("") {}
+Client::Client(int fd) : _fd(fd), _passwordAuthenticated(false), _nickname(""), _username(""), _realname(""), _isUserReceived(false) {}
 Client::~Client() {}
-Client::Client(const Client &other) : _fd(other._fd), _readBuf(other._readBuf), _passwordAuthenticated(other._passwordAuthenticated), _isUserReceived(other._isUserReceived), _nickname(other._nickname), _username(other._username), _realname(other._realname) {}
+Client::Client(const Client &other) : _fd(other._fd), _readBuf(other._readBuf), _passwordAuthenticated(other._passwordAuthenticated), _nickname(other._nickname), _username(other._username), _realname(other._realname), _isUserReceived(other._isUserReceived) {}
 Client &Client::operator=(const Client &other)
 {
 	if (this != &other)
@@ -32,7 +36,6 @@ int Client::getFd() const
 	return _fd;
 }
 
-#include "../includes/Server.hpp"
 // methods
 void Client::appendToReadBuf(const std::string &data, size_t len)
 {
@@ -67,47 +70,14 @@ void Client::setPasswordAuthenticated(bool authenticated)
 	_passwordAuthenticated = authenticated;
 }
 
-bool	Client::isValidNickname(const std::string &nickname)
-{
-	// Check if the nickname is empty
-	if (nickname.empty())
-		return false;
-
-	// Check if the nickname starts with a letter or special character
-	if (!std::isalpha(nickname[0]) && !std::ispunct(nickname[0]))
-		return false;
-
-	// Check if the nickname contains only valid characters (letters, digits, and special characters)
-	for (size_t i = 1; i < nickname.size(); ++i)
-	{
-		if (!std::isalnum(nickname[i]) && !std::ispunct(nickname[i]))
-			return false;
-	}
-
-	return true;
-}
-
 // nickname getter and setter
-const std::string Client::getNickname() const
+std::string Client::getNickname() const
 {
 	return _nickname;
 }
 void Client::setNickname(const std::string &nickname)
 {
 	_nickname = nickname;
-}
-bool Client::isNicknameInUse(const std::string &nickname)
-{
-	// Check if the nickname is already in use by any client
-	for (std::map<int, Client *>::iterator it = Server::_clients.begin(); it != Server::_clients.end(); ++it)
-	{
-		if (it->second && it->second->getFd() != _fd) // Exclude the current client
-		{
-			if (it->second->getNickname() == nickname)
-				return true;
-		}
-	}
-	return false;
 }
 
 // username setter
